fa9eb7ccf7ef81f80daf06b2a03e6da24e677317
Joshua Kim <joshua0702@gmail.com>
2022-11-05 17:09:50 -0700

    refactor to include circom

---
+++ forge/src/WorkAgreement.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.8.15;
+
+contract WorkAgreement {
+  enum State {
+    LIVE,
+    CANCELLED 
+  }
+
+	struct Agreement {
+    address issuer;
+		address recipient;
+		uint startDate;
+		uint endDate;
+    bytes32 role; // bytes 32 string, assume ENUM? to keep simple?
+		bytes32 salaryFromHash; // hash(secretEmpr, salary)
+		State state; // did employee accepted
+	}
+
+  struct AgreementInput {
+		address recipient;
+		uint startDate;
+		uint endDate;
+    bytes32 role; // bytes 32 string, assume ENUM? to keep simple?
+		bytes32 salaryFromHash; // hash(secretEmpr, salary)
+	}
+
+	mapping(address => mapping(address => uint)) public pairToId;
+	mapping(address => uint[]) public liveIds; // list of parties agreements were issued to
+	mapping(uint => Agreement) public agreements;
+
+  uint nextId = 1;
+
+	function issueAgreement(AgreementInput memory input) external returns (uint newId) {
+    Agreement memory newAgreement = Agreement({
+      issuer: msg.sender,
+      recipient: input.recipient,
+      startDate: input.startDate,
+      endDate: input.endDate,
+      role: input.role, 
+      salaryFromHash: input.salaryFromHash, 
+      state: State.LIVE 
+    });
+
+    newId = nextId++;
+    pairToId[msg.sender][newAgreement.recipient] = newId;
+    agreements[newId] = newAgreement;
+    liveIds[msg.sender].push(newId);
+	}
+
+  function cancelAgreement(uint idToCancel) external {
+    Agreement memory pendingAgreement = agreements[idToCancel];
+    require(
+      pendingAgreement.recipient == msg.sender || pendingAgreement.issuer == msg.sender, 
+      "only issuer and recipient can cancel"
+    );
+
+    agreements[idToCancel].state = State.CANCELLED;
+    uint liveLen = liveIds[pendingAgreement.issuer].length;
+    bool removed;
+    for (uint i; i < liveLen; i++) {
+      if (liveIds[pendingAgreement.issuer][i] == idToCancel) {
+        liveIds[pendingAgreement.issuer][i] = liveIds[pendingAgreement.issuer][liveLen-1];
+        liveIds[pendingAgreement.issuer].pop();
+        removed = true;
+        break;
+      }
+    }
+
+    require(removed, "agreement does not exist");
+  }
+
+}
---
+++ forge/test/WorkAgreementTest.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "src/WorkAgreement.sol";
+
+
+contract WorkAgreementTest is Test {
+  WorkAgreement work;
+
+  /* salaries */
+  uint[30] salaries = [
+    148_250e18, 115_000e18, 138_000e18, 143_000e18, 158_000e18, // des1
+    168_250e18, 135_000e18, 168_000e18, 163_000e18, 178_000e18, // des2
+    105_250e18, 110_000e18, 95_000e18, 79_000e18, 45_000e18, // eng1
+    145_250e18, 156_000e18, 200_000e18, 179_000e18, 145_000e18, // eng2
+    100_250e18, 99_000e18, 88_000e18, 104_000e18, 105_000e18, // mar1
+    120_250e18, 109_000e18, 134_000e18, 123_000e18, 115_000e18
+  ]; // mar2
+
+  /* addresses are simply vm.addr(i) in ascending order similar to salaries */
+  uint secret = 12345; // used to hash salaries
+
+  function testRun() external {
+    _setup();
+    _issueAgreements();
+  }
+
+  function _setup() public {
+    vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+    work = new WorkAgreement();
+    vm.stopBroadcast();
+  }
+
+  function _issueAgreements() public {
+    WorkAgreement.AgreementInput memory input;
+    for (uint i; i < 30; i++) {
+      bytes32 role;
+      if (i < 5) { role = "DESIGNER_1"; } 
+      else if (i >= 5 && i < 10) { role = "DESIGNER_2"; }
+      else if (i >= 10 && i < 15) { role = "ENGINEER_1"; }
+      else if (i >= 15 && i < 20) { role = "ENGINEER_2"; }
+      else if (i >= 20 && i < 25) { role = "MARKETING_1"; }
+      else { role = "MARKETING_2"; }
+
+      console2.logBytes32(role);
+      console2.log(i, salaries[i]);
+
+      input = WorkAgreement.AgreementInput({
+        recipient: vm.addr(i+1), // first one is the employer
+        startDate: block.timestamp, // can change later
+        endDate: 0,
+        role: role,// bytes 32 string, assume ENUM? to keep simple?
+        salaryFromHash: keccak256(abi.encodePacked(secret, salaries[i])) // hash(secret, salary)
+      });
+      vm.startBroadcast(vm.envUint("DAO_PRIVATE_KEY"));
+      work.issueAgreement(input);
+      vm.stopBroadcast();
+    }
+  }
+ }
